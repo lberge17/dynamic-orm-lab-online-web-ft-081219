96ac1ec368c266993ad59f9e00e1f97f29a8113b
Laura Berge <lauraeberge@gmail.com>
2019-09-22 16:57:12 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute_hash)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys[0].to_s} = #{attribute_hash.values[0]}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys[0].to_s} = #{attribute_hash.values[0].to_s}"
 
     DB[:conn].execute(sql)
   end
