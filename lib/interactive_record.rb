5d9896fe6feb8ab15c3f6ea02fb912631cc3468f
Laura Berge <lauraeberge@gmail.com>
2019-09-22 16:26:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,8 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = true
     
-    DB[:conn].execute("pragma table_info('#{table_name}')")
+    table_info = DB[:conn].execute("pragma table_info('#{table_name}')")
+    
   end
   
 end
